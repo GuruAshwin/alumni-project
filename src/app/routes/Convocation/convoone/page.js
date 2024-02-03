289c81584f7aaf402673b6ae2937ef2f903e4069
Ashwin B <guruashwin5537@gmail.com>
2024-02-03 10:08:59 +0530

    convo commit

--- src/app/routes/Convocation/convoone/page.js
+++ src/app/routes/Convocation/convoone/page.js
@@ -1,23 +1,22 @@
 "use client";
 import React from "react";
+import "./convoone.css";
 
-import "./convo2017.css";
+export default function ConvocationOne() {
+  const images = [
+    "/convocation/2017/IMG_0361.jpg",
+    "/convocation/2017/IMG_0368.JPG",
+    "/convocation/2017/IMG_0370.JPG",
+    // Add more image paths as needed
+  ];
 
-
-export default function convovation2017(){
-
-    return(
-        <div className="Main-contianer">
-            <div className="image-contianer">
-                <img src="/convocation/2017/IMG_0361.jpg"></img>
-            </div>
-            <div className="image-contianer">
-                <img src="/convocation/2017/IMG_0368.JPG"></img>
-            </div>
-            <div className="image-contianer">
-                <img src="/convocation/2017/IMG_0370.JPG"></img>
-            </div>
+  return (
+    <div className="Main-container">
+      {images.map((image, index) => (
+        <div key={index} className="image-container">
+          <img src={image} alt={`Image ${index + 1}`} />
         </div>
-    )
+      ))}
+    </div>
+  );
 }
-
