563b73089c153f2e157c1349aef67dec037b4fd5
Ashwin B <guruashwin5537@gmail.com>
2024-02-18 21:23:48 +0530

    css file name change

--- src/app/components/testimonial_slider/Testimonialtemplate.js
+++ src/app/components/testimonial_slider/Testimonialtemplate.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from 'react';
-import "./Testimonialslider.css";  // Make sure this path is correct
+import "./TestimonialSlider.css";  // Make sure this path is correct
 
 
 
