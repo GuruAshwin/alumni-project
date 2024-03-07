0b5b18612b53cb055a490a6b92ef4f4f01f450f1
Ashwin B <guruashwin5537@gmail.com>
2024-03-07 10:45:06 +0530

    mrw4zxrexcy

--- src/app/components/testimonial_slider/Testimonialtemplate.js
+++ src/app/components/testimonial_slider/Testimonialtemplate.js
@@ -35,7 +35,7 @@ export function TestimonialSlider(){
     }, 5000); // Change slide every 5 seconds
 
     return () => clearInterval(intervalId);
-  }, [testimonialData.length]);
+  }, [testimonialData]);
 
   return (
     <div className="testimonial-slider">
